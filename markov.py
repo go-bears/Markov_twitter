7f4f5aee25db438e6883bc44dfdd3df1361c494b
Hackbright Student <info@hackbrightacademy.com>
2016-01-13 17:55:35 -0800

    working twitter fun

--- markov.py
+++ markov.py
@@ -15,6 +15,7 @@ def open_and_read_file(filenames):
         body = body + text_file.read()
         text_file.close()
 
+    #print body
     return body
 
 
@@ -23,11 +24,12 @@ def make_chains(text_string):
 
     chains = {}
 
-    words = text_string.split()
+    words = text_string.strip().split()
+    #print words
 
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
+    for i in range(len(words) - 4):
+        key = (words[i], words[i + 1], words[i + 2], words[i + 3])
+        value = words[i + 4]
 
         if key not in chains:
             chains[key] = []
@@ -37,29 +39,47 @@ def make_chains(text_string):
         # or we could replace the last three lines with:
         #    chains.setdefault(key, []).append(value)
 
+    import pprint; pprint.pprint(chains)
     return chains
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    key = choice(chains.keys())
-    words = [key[0], key[1]]
+    
+
     text = ""
-    while key in chains and len(text) < 130:
+    key = choice(chains.keys())     
+    words = [key[0], key[1], key[2], key[3]]
+
+ 
+    capital_list = [item for item in chains.keys() if item[0].isupper()]
+    punctuation_ending_list = [item for item in chains.keys() if not item[3][-1].isalpha()]
+    punctuation_ending_tuple = " ".join(choice(punctuation_ending_list))
+
+    if len(text) < 1:
+        capital_word = choice(capital_list)
+        text = " ".join(capital_word)
+
+
+    while key in chains and len(text) < 100:
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text)
         #
         # Note that for long texts (like a full book), this might mean
         # it would run for a very long time.
-        
+
         word = choice(chains[key])
         words.append(word)
         key = (key[1], word)
+        text = text + " " + " ".join(words)
 
-        text = " ".join(words)
+    text = text + " " + punctuation_ending_tuple
+    
 
-    print text
+
+
+    import pprint; pprint.pprint(text)
     print "this is the length", len(text)
     return text
 
@@ -82,11 +102,12 @@ def tweet(chains):
 filenames = sys.argv[1:]
 
 # Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+body = open_and_read_file(filenames)
 
 # Get a Markov chain
-chains = make_chains(text)
+chains = make_chains(body)
 text = make_text(chains)
+# print text
 
 # Your task is to write a new function tweet, that will take chains as input
-tweet(text)
+#tweet(text)
